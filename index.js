fbc1b6d66ca40658735c9a1ca557713b6a159841
MostafaKamalAsif <mostafakamal483905@gmail.com>
2026-05-01 18:12:18 +0600

    working on file system

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 // dependancies
 const { log } = require('console');
 const http = require('http');
+const data = require('./lib/data');
 const { handleReqRes } = require('./helpers/handleReqRes');
 const envionment = require('./enviornment');
 // app object-module scaffolding
 const app = {};
 
+// Testing file system
+// data.create('Test', 'Newfile', { name: 'Mostafa kamal', Age: '19' }, (err) => {
+//     log(`Error was ${err}`);
+// });
+// read file system
+data.read('Test', 'Newfile', (err, reasult) => {
+    log(err, reasult);
+});
+// updated file system
+data.update('Test', 'Newfile', { name: 'Omer faruk', Age: '23' }, (err) => {
+    log(`Error was ${err}`);
+});
 // create server
 app.CreateServer = () => {
     const server = http.createServer(app.handleReqRes);
